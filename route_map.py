5d6f087cce5108c3dee695af814a86bab893c00a
TalhaAkgul <talhaakgul13@gmail.com>
2025-05-21 00:51:57 +0200

    Remove unnecessary files

--- route_map.py
+++
@@ -1,7 +0,0 @@
-import osmnx as ox
-
-# Get walkable network of Copenhagen
-G = ox.graph_from_place("Copenhagen, Denmark", network_type="walk")
-#ox.save_graphml(G, "copenhagen_walk.graphml")
-
-ox.plot_graph(G)
